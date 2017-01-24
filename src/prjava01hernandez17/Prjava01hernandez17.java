e3fefbb16379b3c51633b2cf153ae64113447281
ubu1 <ubu1@ubu1-david16.dom16david.com>
2017-01-24 17:06:45 +0100

    versió 0.1 del projecte prjava01hernandez17

---
+++ src/prjava01hernandez17/Prjava01hernandez17.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01hernandez17;
+
+/**
+ *
+ * @author hernandez17
+ */
+import java.io.*;
+
+
+public class Prjava01hernandez17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        File f = new File("fitxer01hernandez17.html");
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+            bw.write("<html>"); 
+            bw.newLine();
+            bw.write("  <head>"); 
+            bw.newLine();
+            bw.write("      <title>"); 
+            bw.newLine();
+            bw.write("         Nova pagina web de hernandez17"); 
+            bw.newLine();
+            bw.write("      </title>"); 
+            bw.newLine();
+            bw.write("  </head>"); 
+            bw.newLine();
+            bw.write("  <body>"); 
+            bw.newLine();
+            bw.write("         Nova pagina web de hernandez17"); 
+            bw.newLine();
+            bw.write("  </body>"); 
+            bw.newLine();
+            bw.write("</html>"); 
+            bw.newLine();
+            bw.close();
+        }
+    }
+    
+}
